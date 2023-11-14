c9fe9374905ab7a28c5e95ceb48dd1fae2ceb23c
Fernanda Osorio <A01026502@tec.mx>
2023-11-14 14:41:20 -0600

    mas o menos pero más menos

--- Roomba/agent.py
+++ Roomba/agent.py
@@ -26,6 +26,12 @@ class Roomba(Agent):
         charging_neighbors = []
         unvisited_neighbors = []
 
+    def lowerBattery(self):
+        """
+        Lowers the battery of the agent by 1
+        """
+        self.battery -= 1
+
     def move(self):
         """ 
         Determines if the agent can move in the direction that was chosen
@@ -51,7 +57,7 @@ class Roomba(Agent):
         self.visited_cells.add(self.pos)
         self.model.grid.move_agent(self, next_move)
         self.steps_taken += 1
-        self.battery -= 1
+        self.lowerBattery()
  
     
     
@@ -60,13 +66,62 @@ class Roomba(Agent):
         Detects if there is trash in the same cell as the agent
         """
         cell_contents = self.model.grid.get_cell_list_contents(self.pos)
+    
+        # Check if there is a TrashAgent in the cell
         trash_agents = [agent for agent in cell_contents if isinstance(agent, TrashAgent)]
+        print("trash", trash_agents)
 
         if trash_agents:
-            # If there is trash in the cell, "delete" the TrashAgent
-            trash_agent = trash_agents[0]  # Assuming there is at most one trash agent in the cell
-            self.model.grid.remove_agent(next_move)
+            # Assuming there is at most one trash agent in the cell
+            trash_agent = trash_agents[0]
+            # "Clean up" the trash agent
+            self.model.grid.remove_agent(trash_agent)
+            self.lowerBattery()
+
     
+    def detectObstacle(self):
+        """Detect the Obstacle Agents in neighboring cells and avoid them"""
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True, # Boolean for whether to use Moore neighborhood (including diagonals) or Von Neumann (only up/down/left/right).
+            include_center=False) 
+        # Boolean for whether to include the center cell itself as one of the neighbors
+
+        # obstacle_neighbors = [neighbor for neighbor in possible_steps if neighbor not in self.visited_cells]
+        # next_move = self.random.choice(obstacle_neighbors)
+        # cell_contents = self.model.grid.get_cell_list_contents(next_move)
+        # obstacle_agents = [agent for agent in cell_contents if isinstance(agent, ObstacleAgent)]
+        # if obstacle_agents:
+        #     print("obstacle", obstacle_agents)
+        #     self.model.grid.move_agent(self, next_move)
+        #     self.visited_cells.add(self.pos)
+        #     self.steps_taken += 1
+        #     self.lowerBattery()
+        # else:
+        #     self.model.grid.move_agent(self, next_move)
+        #     self.visited_cells.add(self.pos)
+        #     self.steps_taken += 1
+        #     self.lowerBattery()
+            
+    def ExploreCell(self):
+        """
+        Detects if there is an obstacle in the same cell as the agent
+        """
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True, # Boolean for whether to use Moore neighborhood (including diagonals) or Von Neumann (only up/down/left/right).
+            include_center=False) 
+        # Boolean for whether to include the center cell itself as one of the neighbors
+
+        free_cell = [neighbor for neighbor in possible_steps if neighbor not in self.visited_cells]
+        next_move = self.random.choice(free_cell)
+        # cell_contents = other.model.grid.get_cell_list_contents(other.pos)
+        # obstacle_agents = [agent for agent in cell_contents if isinstance(agent, ObstacleAgent)]
+        self.model.grid.move_agent(self, next_move)
+        self.visited_cells.add(self.pos)
+        self.steps_taken += 1
+        self.lowerBattery()
+
     def ShortestPathtoTrash(self):
         """
         Finds the shortest path to the nearest trash
@@ -89,8 +144,11 @@ class Roomba(Agent):
         Determines the new direction it will take, and then moves
         """
         self.move()
+        self.detectObstacle()
         self.detectTrash()
-        self.detectCharging()
+        # self.detectCharging()
+        
+        # self.ExploreCell()
 
 class ObstacleAgent(Agent):
     """
